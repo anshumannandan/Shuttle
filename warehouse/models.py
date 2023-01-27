3545070da0da79cbfe9e2d7a7d5b590d59829425
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-01-27 19:28:45 +0530

    warehouse models

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from authentication.models import Business
 
-# Create your models here.
+
+class Warehouse(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='warehouses')
+    location = models.CharField(max_length=255)
+    size = models.FloatField()
+    max_quantity = models.IntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Commodity(models.Model):
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name = 'commodities')
+    category = models.CharField(max_length=255)
+    quantity = models.IntegerField()
