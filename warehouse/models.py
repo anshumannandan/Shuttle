40720bbd6fa3afe0bdda15b214a3faa7f8001344
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-01-28 10:12:02 +0530

    add commodities

--- warehouse/models.py
+++ warehouse/models.py
@@ -6,6 +6,7 @@ class Warehouse(models.Model):
     business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='warehouses')
     location = models.CharField(max_length=255)
     volume = models.FloatField()
+    occupied = models.FloatField(default=0)
 
 
 class Category(models.Model):
